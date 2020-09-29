5e845a428cf756f08efc4df746c046580e463655
ShuvoDas1 <67542645+ShuvoDas1@users.noreply.github.com>
2020-09-30 00:02:36 +0600

    pushed

---
+++ src/components/Bookings/Bookings.js
@@ -0,0 +1,23 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../App';
+
+const Bookings = () => {
+    const [bookings,setBooking] = useState([]);
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    useEffect(()=>{
+        fetch('http://localhost:4000/bookings?email='+loggedInUser.email)
+        .then(res => res.json())
+        .then(data =>setBooking(data))
+    },[])
+
+    return (
+        <div>
+            <h3>Total {bookings.length} Bookings</h3>
+            {
+                bookings.map(booking => <li>email: {booking.email} From:{(new Date(booking.checkIn).toDateString('dd/MM/yyyy'))} To: {(new Date(booking.checkOut).toDateString('dd/MM/yyyy'))}</li>)
+            }
+        </div>
+    );
+};
+
+export default Bookings;
